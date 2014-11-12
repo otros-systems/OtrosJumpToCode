bcd372ffa078b35b10ced3ffeb29d534b4133fd0
Krzysztof <otros.systems@gmail.com>
2014-11-12 06:31:35 +0100

    Added toogle action

--- src/main/java/pl/otros/intellij/JumpToCode/logic/OtrosJumpToCodeApplicationComponent.java
+++ src/main/java/pl/otros/intellij/JumpToCode/logic/OtrosJumpToCodeApplicationComponent.java
@@ -17,10 +17,10 @@
 package pl.otros.intellij.JumpToCode.logic;
 
 import com.intellij.ide.plugins.PluginManager;
-import com.intellij.openapi.components.ApplicationComponent;
-import com.intellij.openapi.components.PersistentStateComponent;
-import com.intellij.openapi.components.State;
-import com.intellij.openapi.components.Storage;
+import com.intellij.openapi.actionSystem.ActionManager;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.ToggleAction;
+import com.intellij.openapi.components.*;
 import com.intellij.openapi.options.Configurable;
 import com.intellij.openapi.options.ConfigurationException;
 import com.intellij.util.xmlb.XmlSerializerUtil;
@@ -47,6 +47,33 @@ public class OtrosJumpToCodeApplicationComponent implements ApplicationComponent
 
   public OtrosJumpToCodeApplicationComponent() {
     LOGGER.debug("constructed OtrosJumpToCodeApplicationComponent");
+    ToggleAction toggleAction = new ToggleAction("OtrosJumpToCode: Enable") {
+
+
+
+      @Override
+      public boolean isSelected(AnActionEvent anActionEvent) {
+        return config.isEnabled();
+      }
+
+      @Override
+      public void setSelected(AnActionEvent anActionEvent, boolean b) {
+        LOGGER.info("Setting enabled OtrosJumpToCode: "+ b);
+        config.setEnabled(b);
+        HttpServer.getInstance().configure(config);
+
+      }
+
+      @Override
+      public void update(AnActionEvent e) {
+        super.update(e);
+        final boolean selected = this.isSelected(e);
+        LOGGER.info("Updating, selected: " + selected);
+        e.getPresentation().setText("OtrosJumpToCode enabled: " + config.isEnabled());
+      }
+    };
+
+    ActionManager.getInstance().registerAction("OtrosJumpToCode-toggle",toggleAction);
   }
 
   @Nls
