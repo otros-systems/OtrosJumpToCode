1689eb847b42420adc602777dfca4f4fbaab5f5a
Krzysztof <otros.systems@gmail.com>
2016-02-13 07:48:39 +0100

    Find logger call by inspection

--- src/main/java/pl/otros/intellij/JumpToCode/logic/FileUtils.java
+++ src/main/java/pl/otros/intellij/JumpToCode/logic/FileUtils.java
@@ -16,6 +16,8 @@
 
 package pl.otros.intellij.JumpToCode.logic;
 
+import com.intellij.ide.highlighter.JavaClassFileType;
+import com.intellij.ide.highlighter.JavaFileType;
 import com.intellij.ide.plugins.PluginManager;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.editor.colors.EditorColors;
@@ -25,6 +27,7 @@ import com.intellij.openapi.editor.markup.RangeHighlighter;
 import com.intellij.openapi.editor.markup.TextAttributes;
 import com.intellij.openapi.fileEditor.FileEditorManager;
 import com.intellij.openapi.fileEditor.OpenFileDescriptor;
+import com.intellij.openapi.fileTypes.FileType;
 import com.intellij.openapi.module.Module;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.project.ProjectManager;
@@ -32,17 +35,20 @@ import com.intellij.openapi.roots.PackageIndex;
 import com.intellij.openapi.roots.ProjectFileIndex;
 import com.intellij.openapi.roots.ProjectRootManager;
 import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.psi.JavaPsiFacade;
+import com.intellij.psi.PsiClass;
+import com.intellij.psi.PsiFile;
+import com.intellij.psi.search.GlobalSearchScope;
+import com.intellij.psi.search.PsiShortNamesCache;
 import org.apache.log4j.Logger;
 import pl.otros.intellij.JumpToCode.IOUtils;
 import pl.otros.intellij.JumpToCode.Properties;
 import pl.otros.intellij.JumpToCode.model.SourceLocation;
 
 import javax.swing.*;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.lang.reflect.InvocationTargetException;
+import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.Executors;
@@ -56,6 +62,7 @@ public class FileUtils {
 
 
   private static final ScheduledExecutorService scheduledExecutorService = Executors.newScheduledThreadPool(1);
+  private static final int READ_LIMIT = 100*1000;
 
   /**
    * find all matching locations in currently opened projects
@@ -191,6 +198,69 @@ public class FileUtils {
     }
   }
 
+
+  public static String findWholeClass(String clazz) {
+    final PsiShortNamesCache instance = PsiShortNamesCache.getInstance(ProjectManager.getInstance().getDefaultProject());
+    ProjectManager projectManager = ProjectManager.getInstance();
+    Project[] projects = projectManager.getOpenProjects();
+    List<PsiFile> matches = new ArrayList<PsiFile>();
+    for (Project project : projects) {
+      final JavaPsiFacade psiFacade = JavaPsiFacade.getInstance(project);
+      final PsiClass[] classes = psiFacade.findClasses(clazz, GlobalSearchScope.allScope(project));
+      System.out.println("Found " + classes + " java classes");
+      for (PsiClass p:classes){
+        //check if this is source
+        if (p.canNavigateToSource()){
+          final PsiFile containingFile = p.getContainingFile();
+
+//          final OpenFileDescriptor openFileDescriptor = new OpenFileDescriptor(project, containingFile.getVirtualFile(), 1, 1);
+//          if (openFileDescriptor.canNavigateToSource()){
+//            System.out.println("Can navigate to source");
+//            final VirtualFile file = openFileDescriptor.getFile();
+//            return readVirtualFile(file);
+//          }
+          final FileType fileType = containingFile.getFileType();
+          final String defaultExtension = fileType.getDefaultExtension();
+
+          if (fileType instanceof JavaFileType) {
+            JavaFileType file = (JavaFileType) fileType;
+            final VirtualFile virtualFile = containingFile.getVirtualFile();
+            return readVirtualFile(virtualFile);
+          } else if (fileType instanceof JavaClassFileType){
+            JavaClassFileType javaClassFileType = (JavaClassFileType) fileType;
+            //TODO get source of class?
+            return "";
+          } else if (defaultExtension.equals("scala")){
+            final VirtualFile virtualFile = containingFile.getVirtualFile();
+            return readVirtualFile(virtualFile);
+          } else if (fileType.isBinary()){
+            final PsiFile paretntContainingFile = p.getParent().getContainingFile();
+            final FileType paretntContainingFileFileType = paretntContainingFile.getFileType();
+
+            return String.format("Binary file. parent is %s, type is %s :%s ", paretntContainingFile, paretntContainingFileFileType, paretntContainingFileFileType.getDefaultExtension());
+          }
+
+        }
+      }
+
+      final PsiFile[] filesByName = instance.getFilesByName(clazz);
+      System.out.println("Found " + filesByName.length + " files by name");
+
+      final PsiClass[] classesByName = instance.getClassesByName(clazz, GlobalSearchScope.projectScope(project));
+      System.out.println("Found " + classesByName.length + " classes by name");
+
+      final String[] allClassNames = instance.getAllClassNames();
+      System.out.println("Found " + allClassNames.length + " all by name");
+
+      for (PsiClass psiClass : classesByName) {
+        final PsiFile containingFile = psiClass.getContainingFile();
+        matches.add(containingFile);
+        System.out.println("Is in file: " + containingFile.getName());
+      }
+    }
+    return "";
+  }
+
   private static class RemoveHighLighter implements Runnable {
     private Editor editor;
     private RangeHighlighter highlighter;
@@ -207,6 +277,22 @@ public class FileUtils {
         }
       }, false);
     }
+  }
+
+  private static String readVirtualFile(VirtualFile virtualFile){
+    try {
+      final InputStream inputStream = virtualFile.getInputStream();
+      byte[] buff = new byte[1024];
+      int read = 0;
+      ByteArrayOutputStream bin = new ByteArrayOutputStream(inputStream.available());
+      while ((read = inputStream.read(buff))>0 && read < READ_LIMIT){
+        bin.write(buff,0,read);
+      }
+      return new String(bin.toByteArray(), Charset.forName("UTF-8"));
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+    return "";
   }
 
   private static class SourceFile {
--- src/main/java/pl/otros/intellij/JumpToCode/server/JumpToCodeServlet.java
+++ src/main/java/pl/otros/intellij/JumpToCode/server/JumpToCodeServlet.java
@@ -16,6 +16,14 @@
 
 package pl.otros.intellij.JumpToCode.server;
 
+import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.project.ProjectManager;
+import com.intellij.openapi.util.Computable;
+import com.intellij.psi.*;
+import com.intellij.psi.impl.JavaPsiFacadeEx;
+import com.intellij.psi.search.GlobalSearchScope;
+import com.intellij.psi.search.PsiShortNamesCache;
 import org.apache.commons.lang.StringUtils;
 import pl.otros.intellij.JumpToCode.logic.FileCopyUtils;
 import pl.otros.intellij.JumpToCode.logic.FileUtils;
@@ -27,6 +35,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.PrintWriter;
 
 /**
  */
@@ -59,9 +68,83 @@ public class JumpToCodeServlet extends HttpServlet {
       jump(request, response, true);
     } else if (StringUtils.equalsIgnoreCase("content", operation)) {
       content(request, response);
+    } else if (StringUtils.equalsIgnoreCase("all", operation)) {
+      final String wholeClass = FileUtils.findWholeClass(getParameter(request, "c", "className"));
+      response.setContentType("text/plain");
+      if (wholeClass.length() > 0) {
+        response.getWriter().print(wholeClass);
+      } else {
+        response.sendError(HttpServletResponse.SC_NOT_FOUND);
+      }
+    } else if (StringUtils.endsWithIgnoreCase("new", operation)) {
+      try {
+        newOperation(request, response);
+      } catch (Exception e) {
+        e.printStackTrace(response.getWriter());
+      }
     } else {
       error(response, "Unexpected operation");
     }
+  }
+
+  private void newOperation(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    String packageName = getParameter(request, "p", "packageName");
+    String fileName = getParameter(request, "f", "fileName");
+    final String className = getParameter(request, "c", "className");
+    int lineNumber = parseInt(getParameter(request, "l", "lineNumber"), 0);
+//    String project = request.getParameter("project");
+    String module = request.getParameter("module");
+
+    ProjectManager projectManager = ProjectManager.getInstance();
+    Project[] projects = projectManager.getOpenProjects();
+    final String fqcn = packageName + "." + className;
+    final PrintWriter writer = response.getWriter();
+        //System.out;
+    response.setContentType("text/plain");
+    writer.println("Response for " + fqcn);
+    for (final Project project : projects) {
+
+
+      final String result = ApplicationManager.getApplication().runReadAction(new Computable<String>() {
+        public String compute() {
+          final StringBuilder sb = new StringBuilder();
+          final GlobalSearchScope scope = GlobalSearchScope.allScope(project);
+          final PsiClass[] classesByName = PsiShortNamesCache.getInstance(project).getClassesByName(className, scope);
+          final PsiClass aClass = JavaPsiFacadeEx.getInstanceEx(project).findClass(fqcn);
+
+          final JavaRecursiveElementVisitor javaRecursiveElementVisitor = new JavaRecursiveElementVisitor() {
+            @Override
+            public void visitReferenceElement(PsiJavaCodeReferenceElement reference) {
+              super.visitReferenceElement(reference);
+              sb.append("\n");
+              if (reference.getContext() instanceof PsiMethodCallExpression) {
+                PsiMethodCallExpression mc = (PsiMethodCallExpression) reference.getContext();
+                sb.append(mc.getMethodExpression().getReferenceName());
+                sb.append(mc.getFirstChild());
+                final String caller = ((PsiMethod) ((PsiReferenceExpression) mc.getFirstChild()).resolve()).getContainingClass().getQualifiedName();
+                sb.append("\nCaller is ").append(caller);
+                sb.append("\nText: ").append(mc.getText());
+              }
+            }
+          };
+
+          javaRecursiveElementVisitor.visitElement(aClass);
+          sb.append("\nHave ").append(classesByName.length).append(" results for ").append(project.getName());
+          for (PsiClass psiClass : classesByName) {
+            final PsiField[] allFields = psiClass.getAllFields();
+            sb.append("\nHave ").append(allFields.length).append(" fields");
+            for (PsiField field : allFields) {
+              sb.append("\n").append(field.getName()).append(": ").append(field.getType().getCanonicalText());
+            }
+          }
+          return sb.toString();
+        }
+      });
+      writer.println(result);
+
+    }
+
+
   }
 
   private void content(HttpServletRequest request, HttpServletResponse response) throws IOException {
