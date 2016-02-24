5d7591b581d937f42818b911293a381dd20c98b0
Krzysztof <otros.systems@gmail.com>
2016-02-24 02:18:56 +0100

    Extension point

--- src/main/java/pl/otros/intellij/JumpToCode/logic/locator/JavaPisLocator.java
+++ src/main/java/pl/otros/intellij/JumpToCode/logic/locator/JavaPisLocator.java
@@ -1,6 +1,7 @@
 package pl.otros.intellij.jumptocode.logic.locator;
 
 import com.google.common.base.Function;
+import com.google.common.base.Joiner;
 import com.google.common.base.Optional;
 import com.google.common.io.ByteStreams;
 import com.intellij.openapi.application.ApplicationManager;
@@ -12,15 +13,14 @@ import com.intellij.psi.*;
 import com.intellij.psi.impl.JavaPsiFacadeEx;
 import com.intellij.psi.impl.source.PsiClassImpl;
 import com.intellij.psi.impl.source.PsiMethodImpl;
+import org.apache.commons.lang.StringEscapeUtils;
 import org.apache.commons.lang.StringUtils;
 import pl.otros.intellij.jumptocode.model.JumpLocation;
 import pl.otros.intellij.jumptocode.model.PsiModelLocation;
 
 import java.io.IOException;
 import java.nio.charset.Charset;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class JavaPisLocator implements Locator {
 
@@ -44,7 +44,7 @@ public class JavaPisLocator implements Locator {
   public List<String> getContentByMessage(final String fqcn, final String code) {
     final ArrayList<String> list = new ArrayList<String>();
     final List<? extends JumpLocation> locations = findByLogMessage(fqcn, code);
-    for (JumpLocation location : locations) {
+    for (JumpLocation location : locations)
       if (location instanceof PsiModelLocation) {
         final PsiModelLocation psiModelLocation = (PsiModelLocation) location;
         final VirtualFile virtualFile = psiModelLocation.getContainingFile().getContainingFile().getVirtualFile();
@@ -67,29 +67,17 @@ public class JavaPisLocator implements Locator {
             start = newLinesPositions.get(Math.max(newLinesPositions.size() - 5, 0));
           }
           int end = StringUtils.indexOf(content, '\n', methodCallStart + psiModelLocation.getTextLength());
-          String toDisplay = content.substring(start, end);
-          final String[] lines = toDisplay.split("\n");
-          final StringBuilder sb = new StringBuilder();
-          sb.append("\nPath: ").append(((PsiModelLocation) location).getContainingFile().getVirtualFile().getCanonicalPath()).append("\n");
+          final String[] lines = content.substring(start, end).split("\n");
           int maxLines = 15;
-          if (lines.length > maxLines) {
-            int header = 4;
-            for (int i = 0; i < header; i++) {
-              sb.append(lines[i]).append("\n");
-            }
-            sb.append(".......\n.......\n");
-            for (int i = lines.length - maxLines+header+2; i < lines.length - 1; i++) {
-              sb.append(lines[i]).append("\n");
-            }
-          } else {
-            sb.append(toDisplay);
-          }
-          list.add(sb.toString());
+          String toDisplay = "\nPath: " +
+              ((PsiModelLocation) location).getContainingFile().getVirtualFile().getCanonicalPath() +
+              "\n" +
+              Joiner.on("\n").join(abbreviateLines(lines, maxLines, 3));
+          list.add(toDisplay);
         } catch (IOException e) {
           e.printStackTrace();
         }
       }
-    }
     return list;
   }
 
@@ -131,8 +119,12 @@ public class JavaPisLocator implements Locator {
                       String caller = Optional.fromNullable(containingClass.getQualifiedName()).or("");
                       if (caller.contains("Logger") || caller.contains("log4j.Category")) {
                         final List<PsiLiteralExpression> psiLiteralExpressions = literalExpression(mc.getArgumentList().getExpressions());
+
                         for (PsiLiteralExpression psiLiteralExpression : psiLiteralExpressions) {
-                          String text = unwrap(psiLiteralExpression.getText());
+                          String text = psiLiteralExpression.getText();
+                          text = unwrap(text);
+                          text = StringEscapeUtils.unescapeJava(text);
+                          text = withoutArgsPlaceHolders(text);
                           if (code.contains(text)) {
                             final int textOffset = mc.getTextOffset();
                             final int textLength = mc.getTextLength();
@@ -164,7 +156,10 @@ public class JavaPisLocator implements Locator {
   private static List<PsiLiteralExpression> literalExpression(PsiExpression[] expressions) {
     List<PsiLiteralExpression> r = new ArrayList<PsiLiteralExpression>();
     for (PsiExpression e : expressions) {
-      if (e instanceof PsiLiteralExpression) {
+      if (e instanceof PsiMethodCallExpression){
+        final PsiExpressionList argumentList = ((PsiMethodCallExpression) e).getArgumentList();
+        r.addAll(literalExpression(argumentList.getExpressions()));
+      }else if (e instanceof PsiLiteralExpression) {
         r.add((PsiLiteralExpression) e);
       } else if (e instanceof PsiBinaryExpression) {
         //logger.info("message",param);
@@ -178,12 +173,26 @@ public class JavaPisLocator implements Locator {
     return r;
   }
 
-  private static String unwrap(String text) {
+  protected String unwrap(String text) {
     if (text.startsWith("\"") && text.endsWith("\"") && text.length() > 2) {
       return text.substring(1, text.length() - 1);
     } else {
       return text;
     }
+  }
+
+  protected String withoutArgsPlaceHolders(String text) {
+    String[] splited = text.split("(\\{\\})");
+    final Comparator<String> stringComparator = new Comparator<String>() {
+      @Override
+      public int compare(String o1, String o2) {
+        return o2.length() - o1.length();
+      }
+    };
+    Arrays.sort(splited, stringComparator);
+    final String s1 = splited[0];
+    String s = s1.replaceFirst("^\\s*%\\S+","");
+    return StringUtils.substringBefore(s, "%");
   }
 
   public static Optional<PsiElement> extractParent(PsiElement psiElement) {
@@ -196,6 +205,23 @@ public class JavaPisLocator implements Locator {
       }
     } else {
       return Optional.absent();
+    }
+  }
+
+  protected String[] abbreviateLines(String[] lines, int max, int header){
+    if (lines.length <= max || lines.length<10) {
+      return lines;
+    } else {
+      final ArrayList<String> strings = new ArrayList<String>();
+      String[] result = new String[max];
+      System.arraycopy(lines,0,result,0,header);
+      String spaces = StringUtils.getCommonPrefix(new String[]{"                        ",lines[header-1]});
+
+      result[header] = spaces + ".........";
+      result[header+1] = result[header];
+      final int tail = max - header - 2;
+      System.arraycopy(lines,lines.length- tail,result,header+2,tail);
+      return result;
     }
   }
 
